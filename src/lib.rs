2045807d10f7114d99099bb3bbd2fc88e3131c72
dineshpinto <annual.fallout_0z@icloud.com>
2023-06-11 03:56:48 +0200

    Added Approval support

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ fn map_transfers(blk: eth::Block) -> Result<lpp::Transfers, substreams::errors::
         transfers: blk
             .events::<abi::lpp::events::Transfer>(&[&LENS_PROTOCOL_CONTRACT])
             .map(|(transfer, log)| {
-                substreams::log::info!("NFT Transfer seen");
+                substreams::log::info!("LPP transfer");
 
                 lpp::Transfer {
                     trx_hash: log.receipt.transaction.hash.clone(),
@@ -32,22 +32,53 @@ fn map_transfers(blk: eth::Block) -> Result<lpp::Transfers, substreams::errors::
     })
 }
 
-/// Store the total balance of NFT tokens for the specific TRACKED_CONTRACT by holder
+/// Store the total number of LPP transfers by holder
 #[substreams::handlers::store]
 fn store_transfers(transfers: lpp::Transfers, s: StoreAddInt64) {
     log::info!("NFT holders state builder");
     for transfer in transfers.transfers {
         if transfer.from != NULL_ADDRESS {
-            log::info!("Found a transfer out {}", Hex(&transfer.trx_hash));
+            log::info!("Found transfer out {}", Hex(&transfer.trx_hash));
             s.add(transfer.ordinal, generate_key(&transfer.from), -1);
         }
 
         if transfer.to != NULL_ADDRESS {
-            log::info!("Found a transfer in {}", Hex(&transfer.trx_hash));
+            log::info!("Found transfer in {}", Hex(&transfer.trx_hash));
             s.add(transfer.ordinal, generate_key(&transfer.to), 1);
         }
     }
 }
+
+/// Extracts approval events from the contract
+#[substreams::handlers::map]
+fn map_approvals(blk: eth::Block) -> Result<lpp::Approvals, substreams::errors::Error> {
+    Ok(lpp::Approvals {
+        approvals: blk
+            .events::<abi::lpp::events::Approval>(&[&LENS_PROTOCOL_CONTRACT])
+            .map(|(approval, log)| {
+                substreams::log::info!("LPP approval");
+
+                lpp::Approval {
+                    trx_hash: log.receipt.transaction.hash.clone(),
+                    owner: approval.owner,
+                    approved: approval.approved,
+                    token_id: approval.token_id.to_u64(),
+                    ordinal: log.block_index() as u64,
+                }
+            })
+            .collect(),
+    })
+}
+
+/// Store the total number of LPP approvals by holder
+#[substreams::handlers::store]
+fn store_approvals(approvals: lpp::Approvals, s: StoreAddInt64) {
+    for approval in approvals.approvals {
+        log::info!("Found approval {}", Hex(&approval.trx_hash));
+        s.add(approval.ordinal, generate_key(&approval.owner), -1);
+    }
+}
+
 
 fn generate_key(holder: &Vec<u8>) -> String {
     return format!("total:{}:{}", Hex(holder), Hex(LENS_PROTOCOL_CONTRACT));
